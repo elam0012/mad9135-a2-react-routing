123d0bcd2debb8ec72a9e445518597e8d3fab335
Yasir Elamin <elam0012@algonquinlive.com>
2022-10-10 03:07:35 -0400

    converted unix time to local time

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,10 +1,20 @@
 import React from "react"
 
 export default function Cards({data}) {
-  console.log(data.current.temp)
+  // console.log(data.hourly.slice(0,6))
+
+  // let hourlyData = data.hourly.slice(0,6)
+  // console.log("yasir", hourlyData)
+
   return(
     <div>
-      <p>{data.current.temp}</p>
+      <h2>Current Weather</h2>
+      <p>Temp: {data.current.temp}</p>
+      <p>Real Feel: {data.current.feels_like}</p>
+      <p>Sunrise: {new Date(data.current.sunrise * 1000).toLocaleString("en-CA")}</p>
+      <p>Sunset: {new Date(data.current.sunset * 1000).toLocaleString("en-CA")}</p>
+      <h2>Next 6 Hours</h2>
+      {/* <p>Sunset: {hourlyData.map((data) => data.temp)}</p> */}
     </div>
   )
 }
